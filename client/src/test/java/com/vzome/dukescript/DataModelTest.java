5f159a61c5705b00989fb79ebfec8b41972ddbb1
Scott Vorthmann <scott@vorthmann.org>
2016-07-09 15:59:32 -0700

    Initial state, as generated from DukeScript Maven archetype
    Command used:
       mvn archetype:generate -DarchetypeGroupId=com.dukescript.archetype -DarchetypeArtifactId=knockout4j-archetype -DarchetypeVersion=0.13 -Dwebpath=client-web
    
    responses:
     Define value for property 'groupId': : com.vzome
    Define value for property 'artifactId': : vzome-dukescript
    Define value for property 'version':  1.0-SNAPSHOT: :
    Define value for property 'package':  com.vzome: : com.vzome.dukescript

---
+++ client/src/test/java/com/vzome/dukescript/DataModelTest.java
@@ -0,0 +1,25 @@
+package com.vzome.dukescript;
+
+import net.java.html.junit.BrowserRunner;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Tests for behavior of your application in real systems. The {@link BrowserRunner}
+ * selects all possible presenters from your <code>pom.xml</code> and
+ * runs the tests inside of them.
+ *
+ * See your <code>pom.xml</code> dependency section for details.
+ */
+@RunWith(BrowserRunner.class)
+public class DataModelTest {
+    @Test public void testUIModelWithoutUI() {
+        Data model = new Data();
+        model.setMessage("Hello World!");
+        
+        java.util.List<String> arr = model.getWords();
+        assertEquals("Six words always", arr.size(), 6);
+        assertEquals("Hello is the first word", "Hello", arr.get(0));
+        assertEquals("World is the second word", "World!", arr.get(1));
+    }
+}
